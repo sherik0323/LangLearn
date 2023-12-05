c655398fa2e9946543f52f9de307c601ce175ec9
Sherozbek <sherozradjabov.2004@gmail.com>
2023-12-05 09:53:14 +0500

    Initial Commit

---
+++ langLearn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  langLearn
+//
+//  Created by Sherozbek on 05/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ langLearn/langLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  langLearnApp.swift
+//  langLearn
+//
+//  Created by Sherozbek on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct langLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
